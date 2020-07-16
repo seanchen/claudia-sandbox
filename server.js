b4049875eb8e0c387e20d8009ee05ce4b5ac5462
Sean Chen <sean.chen@leocorn.com>
2020-07-16 17:00:04 -0400

    fix the typo

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 let ApiBuilder = require('claudia-api-builder');
+let superb = require('superb');
 let api = new ApiBuilder();
 
 module.exports = api;
 
 api.get('/hello', function() {
 
-    return 'Hello Claudia.js';
+    return '<h1>Hello Claudia.js</h1>';
+}, {success: { contentType: 'text/html'}});
+
+api.get('/superb', function(request) {
+
+    return request.queryString.name + ' is ' + superb();
 });
